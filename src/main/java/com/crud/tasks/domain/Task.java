f2792496fe495b6c3776aa620a140337b9a145bc
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-05-17 16:00:50 +0200

    upd

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private Long id;
 
     @Column(name = "name")
