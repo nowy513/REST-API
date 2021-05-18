94bcb71b2dd3cdbc87f7d21985afb3b66cb7b176
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-05-18 15:23:12 +0200

    23.1

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -18,9 +18,19 @@ public class TrelloController  {
     private final TrelloClient trelloClient;
 
     @GetMapping("getTrelloBoards")
-    public List<TrelloBoardDto> getTrelloBoards() {
-        return trelloClient.getTrelloBoards();
+//    public List<TrelloBoardDto> getTrelloBoards() {
+//        return trelloClient.getTrelloBoards();
+//    }
+
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+        });
     }
+
     @PostMapping("createTrelloCard")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
         return trelloClient.createdNewCard(trelloCardDto);
