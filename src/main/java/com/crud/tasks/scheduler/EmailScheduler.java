2109c297e12c856487a605db465509aa14e40546
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-05-19 14:25:29 +0200

    23.4

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -17,15 +17,19 @@ public class EmailScheduler {
     private final TaskRepository taskRepository;
     private final AdminConfig adminConfig;
 
-//    @Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "0 0 10 * * *")
+//        @Scheduled(fixedDelay = 10000)
     public void sendInformationEmail(){
         long size = taskRepository.count();
+        String quantity = "task";
+        if(size > 1) {
+            quantity = "tasks";
+        }
         simpleEmailService.send(
                 new Mail(
                         adminConfig.getAdminMail(),
                         SUBJECT,
-                        "Currently in database you got: " + size + " tasks",
+                        "Currently in database you got: " + size + quantity,
                         null
                 )
         );
