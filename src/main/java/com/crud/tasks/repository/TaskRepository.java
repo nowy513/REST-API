04c1a51793592136c61ff6d1432e17bf6387866e
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-07-12 15:09:57 +0200

    Add

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,18 +1,19 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface TaskRepository extends CrudRepository<Task,Long> {
-    @Override
-    List<Task> findAll();
-    @Override
-    Task save(Task task);
-    @Override
-    Optional<Task> findById(Long id);
-    @Override
-    void deleteById(Long id);
+public interface TaskRepository extends JpaRepository<Task,Long> {
+//    @Override
+//    List<Task> findAll();
+//    @Override
+//    Task save(Task task);
+//    @Override
+//    Optional<Task> findById(Long id);
+//    @Override
+//    void deleteById(Long id);
 }
