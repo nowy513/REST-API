845db92d5919bc6bd0090a4e390ffb4755265c91
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-07-15 13:08:07 +0200

    32.1. Thymeleaf - poprawki

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
-import java.util.Objects;
 
 @Controller
 public class StaticWebPageController {
